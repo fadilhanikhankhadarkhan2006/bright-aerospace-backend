3f71136ec9b9a770ffff8c9fe8c28594089917da
Fadil Hani Khan K <fadilhanikhankhadarkhan2006@gmail.com>
2026-02-26 21:55:46 +0530

    Enhance User model with email and validation
    Added email field with validation to User model.

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+from .validators import validate_strong_password
 
 class User(AbstractUser):
     ROLE_CHOICES = (
@@ -13,6 +16,18 @@ class User(AbstractUser):
         choices=ROLE_CHOICES,
         default='student'
     )
+
+
+     email = models.EmailField(unique=True)
+
+    def clean(self):
+        super().clean()
+        try:
+            validate_email(self.email)
+        except ValidationError:
+            raise ValidationError("Invalid email address.")
+        validate_strong_password(self.password)
+        
 
     def __str__(self):
         return f"{self.username} - {self.role}"
