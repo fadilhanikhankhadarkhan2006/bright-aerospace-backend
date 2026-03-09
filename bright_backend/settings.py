1897048b5ae57093659aabc94cc8d1ccf4d08759
Gopi Krishnan K <2005k.gopikrishnan@gmail.com>
2026-03-09 09:15:34 +0530

    Secured the keys

--- bright_backend/settings.py
+++ bright_backend/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u*zd7@y74tf78ppb!ntysu)mzhi9wsu_ze+ui$*1)py6koajyk'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -85,9 +90,9 @@ WSGI_APPLICATION = 'bright_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bright_aerospace',
-        'USER': 'postgres',
-        'PASSWORD': 'BrightAs26',
+        'NAME': os.getenv("DBNAME"),
+        'USER': os.getenv("DBUSER"),
+        'PASSWORD': os.getenv("DBPASSWORD"),
         'HOST': 'localhost',
         'PORT': '5432',
     }
